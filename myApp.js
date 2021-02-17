5e4995c97c1fb11b4d48ef0e9624a1a6dd20b359
Harshal Patil <Harshal1046@Gmail.com>
2021-02-17 16:46:18 +0530

    Served Json on Specific Route

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res) {
     res.sendFile(absolutePath)
   })
 
+//
+  app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"});
+  })
 
 // To Serve Css File
 app.use("/public", express.static(__dirname + "/public"));
