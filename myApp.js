0e59381c14cea8cdef4a7a1bbc41ae0eac797e03
Harshal Patil <Harshal1046@Gmail.com>
2021-03-09 14:52:22 +0530

    Added .env File to Configure App

--- myApp.js
+++ myApp.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var app = express();
 
+
+/** 1) Meet the node console. */
+console.log('Hello World')
+
+
+/** 2) A first working Express Server */
+app.get('/',function(req,res) {
+res.send("Hello Express");
+})
+
+
+/** 3) Serve an HTML file */
 app.get("/", function(req, res) {
     var absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath)
   })
 
-//
-  app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
-  })
-
-// To Serve Css File
+  
+/** 4) Serve static assets  */
 app.use("/public", express.static(__dirname + "/public"));
 
 
+/** 5) serve JSON on a specific route */
+  // app.get("/json", function(req, res) {
+  //   res.json({"message": "Hello json"});
+  // })
+
+
+/** 6) Use the .env file to configure the app */
+app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE == 'uppercase'){
+    res.json({"message": "HELLO JSON"});
+  }
+  res.json({"message": "Hello json"});
+})
 
 
 
